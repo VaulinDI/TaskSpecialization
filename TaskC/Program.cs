b9849cd19fbcea3fb3bec677e4b820a20396e000
VaulinDI <vdi85@mail.ru>
2022-09-14 20:26:18 +0300

    task solved

---
+++ TaskC/Program.cs
@@ -0,0 +1,22 @@
+﻿void ChangeString(string[] s)
+{
+    string[] change = new string[s.Length];
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (s[i].Length < change.Length)
+        {
+            change[i] = s[i];
+            Console.Write(change[i] + "  ");
+        }
+    }
+}
+
+string[] s1 = { "Hello", "2", "world", ":-)" };
+string[] s2 = { "1234", "1567", "-2", "computer science"};
+string[] s3 = { "Russia", "Denmark", "Kazan"};
+
+ChangeString(s1);
+Console.WriteLine();
+ChangeString(s2);
+Console.WriteLine();
+ChangeString(s3);
